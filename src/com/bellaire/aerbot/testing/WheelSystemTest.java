7a4a2f85726b60f97586028d3ac17c705d3972f2
Killin-A13 <amanuelbayu@gmail.com>
2014-11-05 17:26:41 -0600

    almost done with wheelSystem test

--- src/com/bellaire/aerbot/testing/WheelSystemTest.java
+++ src/com/bellaire/aerbot/testing/WheelSystemTest.java
@@ -29,7 +29,7 @@ public class WheelSystemTest {
 	 */
 	@Test
 	public void test() {
-		InputMethod input = new InputMethod(){
+		InputMethod input1 = new InputMethod() { // test move value
 
 			@Override
 			public double getLeftX() {
@@ -95,28 +95,184 @@ public class WheelSystemTest {
 			public boolean straightDrive() {
 				return true;
 			}
-			
+
 		};
-		
+
 		Assert.assertTrue(wheelSystem.getCurrentLeftY() == 0);
-		wheelSystem.move(input);
+		wheelSystem.move(input1);
 		Assert.assertTrue(wheelSystem.getCurrentLeftY() == -1);
 		Assert.assertTrue(wheelSystem.getCurrentRampY() == -0.5);
-		
-		Assert.assertTrue(driveXvalue == -0.5);
-		Assert.assertTrue(driveYvalue == 0);
-		
-		wheelSystem.move(input);
+
+		Assert.assertTrue(driveYvalue == -0.5);
+		Assert.assertTrue(driveXvalue == 0);
+
+		wheelSystem.move(input1);
 		Assert.assertTrue(wheelSystem.getCurrentLeftY() == -1);
 		Assert.assertTrue(wheelSystem.getCurrentRampY() == -0.75);
-		
-		
-		
-	}
-	
-	private class TestWheelSystem extends WheelSystem{
 
-		/* (non-Javadoc)
+		Assert.assertTrue(driveYvalue == -0.75);
+		Assert.assertTrue(driveXvalue == 0);
+		
+		InputMethod input2 = new InputMethod() { // test rotate value
+
+			@Override
+			public double getLeftX() {
+				return 0;
+			}
+
+			@Override
+			public double getRightX() {
+				return 1;
+			}
+
+			@Override
+			public double getLeftY() {
+				return 0;
+			}
+
+			@Override
+			public double getRightY() {
+				return 0;
+			}
+
+			@Override
+			public boolean shift() {
+				return false;
+			}
+
+			@Override
+			public boolean catchBall() {
+				return false;
+			}
+
+			@Override
+			public boolean shoot() {
+				return false;
+			}
+
+			@Override
+			public boolean shooterLift() {
+				return false;
+			}
+
+			@Override
+			public int intake() {
+				return 0;
+			}
+
+			@Override
+			public boolean intakeToggle() {
+				return false;
+			}
+
+			@Override
+			public boolean intakeLift() {
+				return false;
+			}
+
+			@Override
+			public boolean directionToggle() {
+				return false;
+			}
+
+			@Override
+			public boolean straightDrive() {
+				return true;
+			}
+
+		};
+
+		Assert.assertTrue(driveXvalue == 0);
+		wheelSystem.move(input2);
+		Assert.assertTrue(driveYvalue == -0.375);
+		Assert.assertTrue(driveXvalue == 1);
+		
+		InputMethod input3 = new InputMethod() { // test shift value
+
+			@Override
+			public double getLeftX() {
+				return 0;
+			}
+
+			@Override
+			public double getRightX() {
+				return 0;
+			}
+
+			@Override
+			public double getLeftY() {
+				return 0;
+			}
+
+			@Override
+			public double getRightY() {
+				return 0;
+			}
+
+			@Override
+			public boolean shift() {
+				return true;
+			}
+
+			@Override
+			public boolean catchBall() {
+				return false;
+			}
+
+			@Override
+			public boolean shoot() {
+				return false;
+			}
+
+			@Override
+			public boolean shooterLift() {
+				return false;
+			}
+
+			@Override
+			public int intake() {
+				return 0;
+			}
+
+			@Override
+			public boolean intakeToggle() {
+				return false;
+			}
+
+			@Override
+			public boolean intakeLift() {
+				return false;
+			}
+
+			@Override
+			public boolean directionToggle() {
+				return false;
+			}
+
+			@Override
+			public boolean straightDrive() {
+				return true;
+			}
+
+		};
+
+		Assert.assertTrue(wheelSystem.getGear() == 0);
+		wheelSystem.move(input3);
+		Assert.assertTrue(wheelSystem.getGear() == 1);
+		wheelSystem.move(input3); // if hold down the gear should stay 1
+		Assert.assertTrue(wheelSystem.getGear() == 1);
+		wheelSystem.move(input2); // the gear should be 1 if you are not sill holding it
+		Assert.assertTrue(wheelSystem.getGear() == 1);
+		wheelSystem.move(input3); // if hold the gear back it should change to 0
+		Assert.assertTrue(wheelSystem.getGear() == 0);
+
+	}
+
+	private class TestWheelSystem extends WheelSystem {
+
+		/*
+		 * (non-Javadoc)
+		 * 
 		 * @see com.bellaire.aerbot.systems.WheelSystem#getCurrentRampY()
 		 */
 		@Override
@@ -124,8 +280,12 @@ public class WheelSystemTest {
 			return super.getCurrentRampY();
 		}
 
-		/* (non-Javadoc)
-		 * @see com.bellaire.aerbot.systems.WheelSystem#updateSmartDashboard(com.bellaire.aerbot.input.InputMethod)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.bellaire.aerbot.systems.WheelSystem#updateSmartDashboard(com.
+		 * bellaire.aerbot.input.InputMethod)
 		 */
 		@Override
 		public void updateSmartDashboard(InputMethod input) {
@@ -168,7 +328,7 @@ public class WheelSystemTest {
 
 		@Override
 		public void straightDrive(double moveValue) throws NullPointerException {
-			driveXvalue = moveValue;
+			driveYvalue = moveValue;
 		}
 
 		@Override
@@ -207,12 +367,12 @@ public class WheelSystemTest {
 
 		@Override
 		public void gearsOff() {
-			super.gearsOff();
+			setGear(0);
 		}
 
 		@Override
 		public void gearsReverse() {
-			super.gearsReverse();
+			setGear(1);
 		}
 
 		@Override
@@ -220,9 +380,7 @@ public class WheelSystemTest {
 			driveYvalue = moveValue;
 			driveXvalue = rotateValue;
 		}
-		
-		
-		
+
 	}
 
 }
